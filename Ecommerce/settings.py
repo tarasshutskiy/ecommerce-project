5e03207e24bb2a2cf6a7943cc14bad59e624ef0d
Taras <taras.shutskyi98@gmail.com>
2021-07-13 00:47:12 +0300

    Corectt staticfiles and add new image

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATICfILES_DIRS = [
-    BASE_DIR / 'ecommerse/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'Ecommerce/static/'
 ]
 
 STATIC_URL = '/static/'
