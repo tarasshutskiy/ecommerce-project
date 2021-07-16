096cf4bf786fe4db41798e0ebd114dc258033a75
Taras <taras.shutskyi98@gmail.com>
2021-07-16 13:11:35 +0300

    Add context_processors.py and new func in ecommerce

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -12,8 +13,17 @@ class Category(models.Model):
         verbose_name = "category"
         verbose_name_plural = 'catygories'
 
+
+    def get_url(self):
+        return reverse("products_by_category", args=[self.slug])
+
+
+
     def __str__(self):
         return self.name
+
+    
+
 
 
 
