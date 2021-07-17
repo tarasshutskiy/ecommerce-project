03df6b0ab726e3731a65d7ed9aa78bc495abbcf5
Taras <taras.shutskyi98@gmail.com>
2021-07-17 21:21:05 +0300

    Add cart.detail ,_cart_id and add_cart on views.py , upgreta bootstrap in catr.html

--- shop/models.py
+++ shop/models.py
@@ -18,13 +18,8 @@ class Category(models.Model):
         return reverse("products_by_category", args=[self.slug])
 
 
-
     def __str__(self):
         return self.name
-
-    
-
-
 
 
 
@@ -52,3 +47,35 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+
+
+
+
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=250, blank =True)
+    date_added = models.DateTimeField(auto_now_add = True)
+    class Meta:
+        ordering = ['date_added']
+        db_table = 'Cart'
+
+    def __str__(self):
+        return self.cart_id
+
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete = models.CASCADE)
+    quantity = models.IntegerField()
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'CartItem'
+
+
+    def sub_total(self):
+        return self.product.price * self.quantity
+
+
+    def __str__(self):
+        return self.product
